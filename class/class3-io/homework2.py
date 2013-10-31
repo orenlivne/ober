1c527cfd42fd22a2bb698d82ba877e6177e8076c
Oren Livne <oren.livne@gmail.com>
2013-10-31 13:11:20 -0700

    Added work from today's class.

--- class/class3-io/homework2.py
+++ class/class3-io/homework2.py
@@ -1,47 +1,37 @@
 #!/usr/bin/env python
-'''
-============================================================
-Homework #2
-- Read a PLINK map file and a corresponding frq file
-into a single list with 5 columns.
-- Filter the list of snp to MAF >= 5% in a bp range
-[a,b]. 
-- Write the result into a file.
-============================================================
-'''
-import sys, os, csv, itertools as it
+import os, sys, csv, itertools as it
 from optparse import OptionParser
 
-####################################################################################
-if __name__ == '__main__':
-    '''
-    --------------------------------------------------
-    Main program
-    --------------------------------------------------
-    '''
-    # Parse and validate command-line arguments
-    PROGRAM = os.path.basename(sys.argv[0])
-    usage = 'Usage: %s <map-file> <freq-file>\n' \
+def parse_command_line(argv):
+    PROGRAM = os.path.basename(argv[0])
+    usage = 'Usage: %s <map-file> <freq-file> <out-file>\n' \
         'Read a PLINK map and freq file. Join and filter by MAF.\n' \
         'Write to standard output.\n' \
         '\nType ''%s -h'' to display full help.' % (PROGRAM, PROGRAM)
     parser = OptionParser(usage=usage)
     parser.add_option('-t', '--min-maf', type='float', dest='min_maf', default=0.05,
                       help='MAF threshold - only SNPs with MAF >= this threshold are returned.')
-#    parser.add_option('-l', '--bp-start', type='long', dest='bp_start', default=0,
-#                      help='MAF threshold - only SNPs with MAF >= this threshold are returned.')
-#    parser.add_option('-r', '--bp-stop', type='long', dest='bp_stop', default=5e8,
-#                      help='MAF threshold - only SNPs with MAF >= this threshold are returned.')
-    options, args = parser.parse_args(sys.argv[1:])
-    if len(args) != 2:
+    options, args = parser.parse_args(argv[1:])
+    if len(args) != 3:
         print usage
         sys.exit(1)
+    return options, args
+
+def process_files(map_file_name, frq_file_name, out_file_name, min_maf=0.05):
+    try:
+        with open(map_file_name, 'rb') as map_file:
+            with open(frq_file_name, 'rb') as frq_file:
+                with open(out_file_name, 'wb') as out_file:
+                    for map_line, frq_line in it.ifilter(lambda (_, y): float(y[4]) >= min_maf,
+                                                        zip(
+                                                            csv.reader(map_file, delimiter='\t', skipinitialspace=True),
+                                                            it.islice(csv.reader(frq_file, delimiter=' ', skipinitialspace=True), 1, None)
+                                                            )):
+                        out_file.write('%2d %-20s %-10d %s/%s %.3f\n' % \
+                                       (int(map_line[0]), map_line[1], int(map_line[3]), frq_line[2], frq_line[3], float(frq_line[4])))
+    except IOError as e:
+        print e
     
-    # Read from files and filter by MAF
-    with open(args[0], 'rb') as map_file:
-         with open(args[1], 'rb') as frq_file:
-            for map_line, frq_line in it.ifilter(lambda (map_line, frq_line): float(frq_line[4]) >= options.min_maf,
-                                                 it.izip(csv.reader(map_file, delimiter='\t', skipinitialspace=True),
-                                                         it.islice(csv.reader(frq_file, delimiter=' ', skipinitialspace=True), 1, None))):
-                print '%2d %-30s %-10d %s/%s %f %d' % (int(map_line[0]), map_line[1], int(map_line[3]),
-                                                frq_line[2], frq_line[3], float(frq_line[4]), int(frq_line[5]))
+if __name__ == '__main__':
+    options, args = parse_command_line(sys.argv)
+    process_files(args[0], args[1], args[2], min_maf=options.min_maf)
---
+++ class/class3-io/program.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import homework2
+
+print 'program:',  __name__
+homework2.useful_function()
